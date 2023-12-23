0f7998b7b0c38e83af6ca5ac4037ec785b5d538a
tekfig <ifduo2022@outlook.com>
2023-12-23 17:31:30 +0800

    init

---
+++ src/main/java/com/nfc/study/action/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.nfc.study.action.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(String name){
+        return "Hello " + name;
+    }
+}
